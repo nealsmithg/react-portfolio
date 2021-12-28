165f53a288c173151f32b2f5ca3973e8840eafd3
Cornelius Smith <nealsmithg@gmail.com>
2021-12-28 10:51:16 -0500

    starting on components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Header, Footer, Project } from "./components";
+
+function App() {
+  return (
+    <div className="bucket-app">
+      <Header />
+      <Project />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Project from "./Project";
+
+const Components = { Footer, Header, Project };
+
+export default Components;
